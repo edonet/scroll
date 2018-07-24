903bc918f68a6d7cae5e3a2e5877bd65cd9abe69
edonet <edonet@163.com>
2018-07-24 17:48:52 +0800

    update: scroll

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# scroll
+# @arted/scroll
 Arted scroll
+
+## Installation
+npm
+``` shell
+$ npm install @arted/scroll
+```
+
+or yarn
+``` shell
+$ yarn add @arted/scroll
+```
+
+## Usage
+``` javascript
+import Scroll from '@arted/scroll';
+
+// 创建滚动
+const scroll = new Scroll('#el');
+
+// 添加滚动事件
+scroll.on('scroll', e => {
+    // do something;
+});
+
+// 销毁滚动
+scroll.destroy();
+```
---
+++ src/app.js
@@ -0,0 +1,65 @@
+/**
+ *****************************************
+ * Created by lifx
+ * Created on 2018-07-15 14:05:50
+ *****************************************
+ */
+'use strict';
+
+
+/**
+ *****************************************
+ * 加载依赖
+ *****************************************
+ */
+import React, { Component, createRef } from 'react';
+import Scroll from '../lib/index';
+import style from './app.scss';
+
+
+/**
+ *****************************************
+ * 定义组件
+ *****************************************
+ */
+export default class App extends Component {
+
+    /* 初始化组件 */
+    constructor(props, ...args) {
+        super(props, ...args);
+
+        // 定义属性
+        this.$$ref = createRef();
+        this.$$scroll = null;
+    }
+
+    /* 渲染列表 */
+    render() {
+        let list = [],
+            len = 100;
+
+        while (len --) {
+            list.unshift(<li key={ len }>{ len }</li>);
+        }
+
+        return (
+            <div className={ style.container } ref={ this.$$ref }>
+                <ul className={ style.list }>{ list }</ul>
+            </div>
+        );
+    }
+
+    /* 挂载完成 */
+    componentDidMount() {
+
+        // 创建滚动对象
+        this.$$scroll = new Scroll(this.$$ref.current);
+
+        // 监听滚动事件
+        this.$$scroll.on('scroll', function (event) {
+            console.log(event.x, event.y, this.x, this.y);
+        });
+
+        console.log(this.$$scroll);
+    }
+}
